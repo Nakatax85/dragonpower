51dbdf2dbc671a5a975e76e07bdc3aecffbcff5b
João Rocha <joaorocha@gmail.com>
2017-06-11 19:35:00 +0100

    Testing the starting screen

---
+++ src/org/earthspecialforces/dragonpower/Testers/FlyingGoku.java
@@ -0,0 +1,25 @@
+package org.earthspecialforces.dragonpower.Testers;
+
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+/**
+ * Created by joaorocha on 11/06/2017.
+ */
+public class FlyingGoku implements KeyboardHandler {
+/*
+    public FlyingGoku{
+
+    }*/
+
+
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+    }
+
+}
