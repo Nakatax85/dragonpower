3ffea91c076ce99f2041c867bdc224a85f73aa84
NJ Silva <njsilva@NJs-MacBook-Pro.local>
2017-06-11 16:19:15 +0100

    Resources package created. Background and inital assets to be added.

---
+++ src/org/earthspecialforces/dragonpower/Testers/Tester.java
@@ -0,0 +1,33 @@
+package org.earthspecialforces.dragonpower.Testers;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+
+/**
+ * Created by njsilva on 09/06/2017.
+ */
+public class Tester {
+    public static void main(String[] args) {
+
+        Rectangle r = new Rectangle(10,10,1080,720);
+        Picture bck = new Picture(10,10,"5090422-wasteland.jpg");
+        Rectangle r2 = new Rectangle(500,300,40,40);
+
+        r.setColor(Color.BLACK);
+        r.fill();
+
+        bck.draw();
+        r2.setColor(Color.WHITE);
+        r2.fill();
+
+
+
+
+
+
+
+
+
+    }
+}
