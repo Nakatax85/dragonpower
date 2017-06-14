61d6bfb4f9f4aacf66c1106adadc2c2a3c34207b
João Rocha <joaorocha@gmail.com>
2017-06-14 22:18:40 +0100

    Physics Engine implemented with gravity

--- src/org/earthspecialforces/dragonpower/game/gameObjects/Player.java
+++ src/org/earthspecialforces/dragonpower/game/gameObjects/Player.java
@@ -8,45 +8,62 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class Player {
 
     private static final int INITIAL_POSITION_X = 110;
-    private static final int INITIAL_POSITICN_Y = 110;
+    private static final int INITIAL_POSITION_Y = 110;
     String imagePath = "imgs/Goku_Cloud_1.png";
 
-
     Picture image;
-    private int positionX;
-    private int positionY;
+    private double positionY;
     private boolean alive;
+    private boolean jumped;
 
-    public Player(){
-        positionX = INITIAL_POSITION_X;
-        positionY = INITIAL_POSITICN_Y;
-        image = new Picture(positionX,positionY,imagePath);
+    public Player() {
+        positionY = INITIAL_POSITION_Y;
+        image = new Picture(INITIAL_POSITION_X, positionY, imagePath);
+
     }
 
-    public void jump(){
-        if (positionY == 0){
-            this.draw(0);
-            return;
+    public void jump() {
+        if (positionY > 38.4) {
+            positionY -= 38.4;
+            image.translate(0, -38.4);
+            image.draw();
+            jumped = true;
         }
-        positionY -= 15;
-        this.draw(-15);
     }
 
-    public void draw(int gravity) {
-        int newgravity = fall(gravity);
-        image.translate(0, newgravity);
+    public void draw(double distance) {
+        //top border
+        if (positionY < 0) {
+            positionY = 0;
+            distance = 0;
+        }
+        //bottom border
+        if (positionY > 400) {
+            positionY = 400;
+            distance = positionY - image.getY();
+        }
+
+        image.translate(0, distance);
         image.draw();
     }
 
-    public int fall(int gravity){
-        if (positionY == 385){
-            return 0;
-        }
-        positionY += gravity;
-        return gravity;
+    public double getPositionY() {
+        return positionY;
     }
 
-    public int getPositionY() {
-        return positionY;
+    public void setPositionY(double positionY) {
+        this.positionY = positionY;
+    }
+
+    public Picture getImage() {
+        return image;
+    }
+
+    public boolean hasJumped() {
+        return jumped;
+    }
+
+    public void setJumped(boolean jumped) {
+        this.jumped = jumped;
     }
 }
