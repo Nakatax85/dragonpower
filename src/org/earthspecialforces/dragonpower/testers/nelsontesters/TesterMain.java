dbea570863c4fc5c1a25a45fa429d6c926aa447b
João Rocha <joaorocha@gmail.com>
2017-06-15 16:48:50 +0100

    Nelson's testers cleared

--- src/org/earthspecialforces/dragonpower/testers/nelsontesters/TesterMain.java
+++ src/org/earthspecialforces/dragonpower/testers/nelsontesters/TesterMain.java
@@ -1,15 +1,8 @@
-package org.earthspecialforces.dragonpower.Testers.nelsontesters;
+package org.earthspecialforces.dragonpower.testers.nelsontesters;
 
 /**
  * Created by njsilva on 13/06/2017.
  */
 public class TesterMain {
-    public static void main(String[] args) {
-
-        TesterGame tg = new TesterGame();
-
-        tg.start();
-
-    }
 
 }
